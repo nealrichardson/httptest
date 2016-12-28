a08326faeeab25d74f5c27e721d4f3a614e7678b
Neal Richardson <neal@crunch.io>
2016-12-28 17:40:30 -0500

    Progress commit toward a simpler fake HTTP

--- tests/testthat/test-fake-http.R
+++ tests/testthat/test-fake-http.R
@@ -6,46 +6,55 @@ public({
             expect_message(g <- GET("http://httpbin.org/get"),
                 "GET http://httpbin.org/get")
             expect_null(content(g))
+            expect_identical(g$url, "http://httpbin.org/get")
         })
         test_that("fakeGET with query", {
-            expect_message(g <- GET("http://httpbin.org/get", query=list(a=1)),
-                "GET http://httpbin.org/get")
+            expect_GET(g <- GET("http://httpbin.org/get", query=list(a=1)),
+                "http://httpbin.org/get")
             expect_equal(content(g), list(a=1))
+            expect_identical(g$url, "http://httpbin.org/get")
         })
         test_that("fakePUT", {
-            expect_message(p <- PUT("http://httpbin.org/get"),
-                "PUT http://httpbin.org/get")
+            expect_PUT(p <- PUT("http://httpbin.org/get"),
+                "http://httpbin.org/get")
             expect_null(content(p))
+            expect_identical(p$url, "http://httpbin.org/get")
         })
         test_that("fakePUT with body", {
-            expect_message(p <- PUT("http://httpbin.org/get", body=list(b=2)),
-                "PUT http://httpbin.org/get")
+            expect_PUT(p <- PUT("http://httpbin.org/get", body=list(b=2)),
+                "http://httpbin.org/get")
             expect_equal(content(p), list(b=2))
+            expect_identical(p$url, "http://httpbin.org/get")
         })
         test_that("fakePATCH", {
-            expect_message(p <- PATCH("http://httpbin.org/get"),
-                "PATCH http://httpbin.org/get")
+            expect_PATCH(p <- PATCH("http://httpbin.org/get"),
+                "http://httpbin.org/get")
             expect_null(content(p))
+            expect_identical(p$url, "http://httpbin.org/get")
         })
         test_that("fakePATCH with body", {
-            expect_message(p <- PATCH("http://httpbin.org/get", body=list(b=2)),
-                "PATCH http://httpbin.org/get")
+            expect_PATCH(p <- PATCH("http://httpbin.org/get", body=list(b=2)),
+                "http://httpbin.org/get")
             expect_equal(content(p), list(b=2))
+            expect_identical(p$url, "http://httpbin.org/get")
         })
         test_that("fakePOST", {
-            expect_message(p <- POST("http://httpbin.org/get"),
-                "POST http://httpbin.org/get")
+            expect_POST(p <- POST("http://httpbin.org/get"),
+                "http://httpbin.org/get")
             expect_null(content(p))
+            expect_identical(p$url, "http://httpbin.org/get")
         })
         test_that("fakePOST with body", {
-            expect_message(p <- POST("http://httpbin.org/get", body=list(b=2)),
-                "POST http://httpbin.org/get")
+            expect_POST(p <- POST("http://httpbin.org/get", body=list(b=2)),
+                "http://httpbin.org/get")
             expect_equal(content(p), list(b=2))
+            expect_identical(p$url, "http://httpbin.org/get")
         })
         test_that("fakeDELETE", {
-            expect_message(d <- DELETE("http://httpbin.org/get"),
-                "DELETE http://httpbin.org/get")
+            expect_DELETE(d <- DELETE("http://httpbin.org/get"),
+                "http://httpbin.org/get")
             expect_null(content(d))
+            expect_identical(d$url, "http://httpbin.org/get")
         })
     })
 })
