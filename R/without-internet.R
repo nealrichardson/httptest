4b2901aca818cf2b24dddde51059a54cef3209d8
Neal Richardson <neal.p.richardson@gmail.com>
2016-12-28 17:24:22 -0500

    Cover fake HTTP with tests

---
+++ R/without-internet.R
@@ -0,0 +1,21 @@
+#' Make all HTTP requests raise an error
+#' @param expr Code to run inside the mock context
+#' @return The result of \code{expr}
+#' @importFrom testthat with_mock
+#' @export
+without_internet <- function (expr) {
+    with_mock(
+        `httr:::request_perform`=stopRequest,
+        # `utils::download.file`=function (url, ...) stop("DOWNLOAD ", url, call.=FALSE),
+        eval.parent(expr)
+    )
+}
+
+stopRequest <- function (req, handle, refresh) {
+    out <- paste(req$method, req$url)
+    body <- req$options$postfields
+    if (!is.null(body)) {
+        out <- paste(out, rawToChar(body))
+    }
+    stop(out, call.=FALSE)
+}
