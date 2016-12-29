daad869a13a3e07cb2ca6e7b06d72ce69f9069c5
Neal Richardson <neal@crunch.io>
2016-12-29 15:44:49 -0500

    Allow expect_VERB not to specify the URL. Switch fakeResponse status based on method and existence of body

--- R/expectations.R
+++ R/expectations.R
@@ -16,46 +16,48 @@
 #' @name expect-verb
 #' @aliases expect_GET expect_POST expect_PUT expect_PATCH expect_DELETE expect_no_request
 #' @export
-expect_GET <- function (object, url, ...) {
+expect_GET <- function (object, url="", ...) {
     expect_mock_request(object, "GET ", url)
 }
 
 #' @rdname expect-verb
 #' @export
-expect_POST <- function (object, url, ...) {
+expect_POST <- function (object, url="", ...) {
     expect_mock_request(object, "POST ", url, " ", ...)
 }
 
 #' @rdname expect-verb
 #' @export
-expect_PATCH <- function (object, url, ...) {
+expect_PATCH <- function (object, url="", ...) {
     expect_mock_request(object, "PATCH ", url, " ", ...)
 }
 
 #' @rdname expect-verb
 #' @export
-expect_PUT <- function (object, url, ...) {
+expect_PUT <- function (object, url="", ...) {
     expect_mock_request(object, "PUT ", url, " ", ...)
 }
 
 #' @rdname expect-verb
 #' @export
-expect_DELETE <- function (object, url) {
+expect_DELETE <- function (object, url="") {
     expect_mock_request(object, "DELETE ", url)
 }
 
 #' @rdname expect-verb
 #' @export
 expect_no_request <- function (object, ...) {
-    ## No request means no error thrown
+    ## No request means no error/message thrown
     request_happened(object, NA)
 }
 
 #' @importFrom testthat expect_error
 expect_mock_request <- function (object, ...) {
-    ## With mock HTTP, POST/PUT/PATCH throw errors with their request info
     expected <- sub(" +$", "", paste0(...)) ## PUT/POST/PATCH with no body may have trailing whitespace
     request_happened(object, expected, fixed=TRUE)
 }
 
+## Without internet, POST/PUT/PATCH throw errors with their request info
+## With fake HTTP, POST/PUT/PATCH print messages with their request info.
+## with_fake_HTTP mocks request_happened to make it expect_message
 request_happened <- testthat::expect_error
