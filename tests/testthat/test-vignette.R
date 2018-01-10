68b13eaaba1bb05d8f434e661aac874eda9cc0d1
Neal Richardson <neal.p.richardson@gmail.com>
2018-01-09 22:08:07 -0800

    start_vignette() takes a path; add change_state()

--- tests/testthat/test-vignette.R
+++ tests/testthat/test-vignette.R
@@ -2,9 +2,27 @@ context("start_vignette")
 
 rp <- httr:::request_perform
 g <- httr::GET
+path <- tempfile()
 
-test_that("start/end_vignette with mocking", {
-    start_vignette()
+test_that("start/end_vignette with recording (dir does not exist yet)", {
+    start_vignette(path)
+    expect_true(identical(rp, httr:::request_perform))
+    expect_false(identical(g, httr::GET))
+    expect_identical(.mockPaths()[1], file.path(path, "0"))
+    ## Test that state_change bumps the mockPath
+    change_state()
+    expect_identical(.mockPaths()[1], file.path(path, "1"))
+    expect_identical(.mockPaths()[2], file.path(path, "0"))
+    end_vignette()
+    expect_true(identical(rp, httr:::request_perform))
+    expect_true(identical(g, httr::GET))
+    expect_false(any(grepl(path, .mockPaths()[1], fixed=TRUE)))
+})
+
+test_that("start/end_vignette with mocking (dir exists)", {
+    on.exit(options(httptest.verbose=NULL))
+    dir.create(path)
+    start_vignette(path)
     expect_false(identical(rp, httr:::request_perform))
     expect_true(identical(g, httr::GET))
     end_vignette()
@@ -12,65 +30,8 @@ test_that("start/end_vignette with mocking", {
     expect_true(identical(g, httr::GET))
 })
 
-test_that("start/end_vignette with recording", {
-    Sys.setenv(RECORD="true")
-    on.exit(Sys.setenv(RECORD=""))
-    start_vignette()
-    expect_true(identical(rp, httr:::request_perform))
-    expect_false(identical(g, httr::GET))
-    end_vignette()
-    expect_true(identical(rp, httr:::request_perform))
-    expect_true(identical(g, httr::GET))
-})
-
-options(httptest.verbose=NULL)  ## Reset default
-
-with_mock_API({
-    test_that("Reading redactors from within a package (and install that package)", {
-        newmocks <- tempfile()
-        expect_message(
-            capture_while_mocking(path=newmocks, {
-                ## Install the "testpkg" to a temp lib.loc _after_ we've already started recording
-                lib <- tempfile()
-                dir.create(lib)
-                Rcmd(c("INSTALL", "testpkg", paste0("--library=", shQuote(lib))),
-                    stdout=NULL, stderr=NULL)
-                library(testpkg, lib.loc=lib)
-                expect_true("testpkg" %in% names(sessionInfo()$otherPkgs))
-
-                r <- GET("http://example.com/get")
-            }),
-            paste0("Using redact.R from ", dQuote("testpkg"))
-        )
-        with_mock_path(newmocks, {
-            r2 <- GET("http://example.com/get")
-        })
-        ## The resulting mock content is what we injected into it from testpkg
-        expect_identical(content(r2), list(fake=TRUE))
-    })
-
-    test_that("Request preprocessing via package inst/httptest/request.R", {
-        ## That function prunes a leading http://pythong.org/ from URLs
-        expect_identical(content(GET("http://pythong.org/api/object1/")),
-            content(GET("api/object1/")))
-    })
-
-    test_that("redact=NULL to override default (and loaded packages)", {
-        expect_true("testpkg" %in% names(sessionInfo()$otherPkgs))
-        ## Great, but let's kill it when we're done
-        on.exit(detach(package:testpkg))
-        newmocks2 <- tempfile()
-        expect_message(
-            capture_while_mocking(simplify=FALSE, path=newmocks2, redact=NULL, {
-                a <- GET("api/", add_headers(`Authorization`="Bearer token"))
-            }),
-            NA
-        )
-        expect_true(any(grepl("Bearer token", readLines(file.path(newmocks2, "api.R")))))
-        skip_on_cran() ## They have a broken R-devel build that chokes on these
-        with_mock_path(newmocks2, {
-            b <- GET("api/", add_headers(`Authorization`="Bearer token"))
-        })
-        expect_equal(content(b), content(a))
+test_that("change_state validation", {
+    with_mock_path("foo", {
+        expect_error(change_state(), "foo is not valid for change_state()")
     })
 })
