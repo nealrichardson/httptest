1fcb2be0fde45a061e3662a3f08f94cf842e69eb
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-20 23:43:11 +0200

    Change the filename `helper.R` to `setup.R` (#45)
    * changed the filename `helper.R` to `setup.R`
    
    * fix

--- tests/testthat/test-use-httptest.R
+++ tests/testthat/test-use-httptest.R
@@ -50,40 +50,40 @@ test_that("add to desc doesn't add if already present", {
         "pkg, httptest")
 })
 
-expect_added_to_helper <- function (str, msg="Adding library\\(httptest\\)") {
+expect_added_to_setup <- function (str, msg="Adding library\\(httptest\\)") {
     f <- tempfile()
     cat(str, file=f)
-    expect_message(add_httptest_to_helper(f), msg)
+    expect_message(add_httptest_to_setup(f), msg)
     expect_true(any(grepl("library(httptest)", readLines(f), fixed=TRUE)))
 }
 
-test_that("add to helper creates file if doesn't exist", {
+test_that("add to setup creates file if doesn't exist", {
     f <- tempfile()
     expect_false(file.exists(f))
-    expect_message(add_httptest_to_helper(f), "Creating")
+    expect_message(add_httptest_to_setup(f), "Creating")
     expect_identical(readLines(f), "library(httptest)")
 })
 
-test_that("add to helper adds", {
-    expect_added_to_helper("")
-    expect_added_to_helper("library(pkg)\n")
+test_that("add to setup adds", {
+    expect_added_to_setup("")
+    expect_added_to_setup("library(pkg)\n")
 })
 
-test_that("add to helper doesn't duplicate", {
-    expect_added_to_helper("library(httptest)\n", msg=NA)
+test_that("add to setup doesn't duplicate", {
+    expect_added_to_setup("library(httptest)\n", msg=NA)
 })
 
 test_that("use_httptest integration test", {
     testpkg <- tempfile()
     dir.create(testpkg)
     expect_error(use_httptest(testpkg), "is not an R package directory")
-    
+
     desc <- file.path(testpkg, "DESCRIPTION")
     cat("Title: Foo\n", file=desc)
-    helper <- file.path(testpkg, "tests", "testthat", "helper.R")
+    setup <- file.path(testpkg, "tests", "testthat", "setup.R")
     expect_message(use_httptest(testpkg))
     expect_identical(readLines(desc), c("Title: Foo", "Suggests: httptest"))
-    expect_identical(readLines(helper), "library(httptest)")
+    expect_identical(readLines(setup), "library(httptest)")
     # It does nothing if you the package already uses httptest
     expect_message(use_httptest(testpkg), NA)
 })
