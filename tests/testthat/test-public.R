0c2e7dbb9cd44075e49f5aa7d6b1cf189109bfde
Neal Richardson <neal.p.richardson@gmail.com>
2020-11-23 15:07:12 -0800

    Better detection of load_all

--- tests/testthat/test-public.R
+++ tests/testthat/test-public.R
@@ -4,7 +4,7 @@ test_that("Functions not exported can be found", {
 
 public({
     test_that("If a function is not exported, the public test context errors", {
-        skip_if(interactive()) # load_all puts everything in the global env
+        skip_if(pkgload::is_dev_package("httptest")) # load_all puts everything in the global env
         expect_error(.internalFunction(),
             'could not find function ".internalFunction"')
     })
