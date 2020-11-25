338f2d22158e50170fc61bdf07872d406cc6f248
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-11-25 14:43:34 +0100

    add replace to signature just in case

--- R/mock-dir.R
+++ R/mock-dir.R
@@ -11,11 +11,13 @@
 #' under `tests/testthat/`
 #' @inheritParams with_mock_api
 #' @inheritParams start_capturing
+#' @param replace Boolean, should the mock directory replace current mock
+#' directories?
 #'
 #' @export
 #'
-with_mock_dir <- function (dir, expr, simplify=TRUE) {
-    with_mock_path(dir, replace=TRUE, {
+with_mock_dir <- function (dir, expr, simplify=TRUE, replace = TRUE) {
+    with_mock_path(dir, replace=replace, {
         if (dir.exists(dir)) {
             ## We already have recorded, so use the fixtures
             with_mock_api(expr)
