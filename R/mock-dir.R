5e7e6c20ace8066da9569817f385ac12c3234911
Neal Richardson <neal.p.richardson@gmail.com>
2020-12-11 15:56:43 -0800

    Doc tweak

--- R/mock-dir.R
+++ R/mock-dir.R
@@ -11,12 +11,12 @@
 #' under `tests/testthat/`
 #' @inheritParams with_mock_api
 #' @inheritParams start_capturing
-#' @param replace Boolean, should the mock directory replace current mock
-#' directories?
+#' @param replace Logical: should the mock directory replace current mock
+#' directories? Default is `TRUE`.
 #'
 #' @export
 #'
-with_mock_dir <- function (dir, expr, simplify=TRUE, replace = TRUE) {
+with_mock_dir <- function (dir, expr, simplify=TRUE, replace=TRUE) {
     with_mock_path(dir, replace=replace, {
         if (dir.exists(dir)) {
             ## We already have recorded, so use the fixtures
