81db537f861eb935610d1c5859a70c22132f0745
Karl Forner <karl.forner@precisionformedicine.com>
2023-01-20 18:58:09 +0100

    dev.

--- tests/testthat/test-trace.R
+++ tests/testthat/test-trace.R
@@ -10,6 +10,36 @@ public({
     expect_error(stop_capturing(), NA)
     expect_error(stop_capturing(), NA)
   })
+
+  FUNS <- list(
+    httr = c('GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'VERB', 'RETRY', 'request_perform', 'body_config'),
+    curl = c('form_file')
+  )
+  
+  .are_pkgs_traced <- function() {
+    .process_fun <- function(fun, pkg) {
+      inherits(getFromNamespace(fun, pkg), "functionWithTrace")
+    }
+    .process_pkg <- function(pkg) {
+      sapply(FUNS[[pkg]], .process_fun, pkg = pkg)
+    }
+    unlist(lapply(names(FUNS), .process_pkg))
+  }
+
+  with_mock_api({
+    test_that("cur/httr functions are properly traced", {
+      browser()
+      expect_true(all(.are_pkgs_traced()))
+    })
+  })
+
+  # test_that("cur/httr functions are properly untraced", {
+  #   for (fun_name in TRACED_FUNCTIONS) {
+  #     fun <- getFromNamespace(fun_name, 'httr')
+  #     expect_false(inherits(fun, "functionWithTrace"))
+  #   }
+  # })
+
 })
 
 test_that("quietly muffles messages, conditional on httptest.debug", {
